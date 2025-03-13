f863efd3b8c86f0b615e59b4f816ba10eb27f982
huusangcv <huusangcv@gmail.com>
2025-03-13 13:12:00 +0700

    feat: add iframe login form accounts.regis.id.vn

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -7,7 +7,6 @@ import classNames from "classnames/bind";
 import { ToastContainer } from "react-toastify";
 import Footer from "../components/footer/Footer";
 import BottomNav from "../components/bottomNavigation/BottomNavigation";
-import Login from "../pages/login/Login";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { useDispatch } from "react-redux";
@@ -51,33 +50,11 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
               <span>×</span>
             </button>
 
-            <div className={cx("content__inner")}>
-              <div className={cx("magic")}></div>
-              <div className={cx("wapper__content")}>
-                <div className={cx("wapper__inner")}>
-                  <header className={cx("header")}>
-                    <a href="https://fullstack.edu.vn" target="_top">
-                      <img
-                        src="/src/assets/logo-mobile.png"
-                        alt="logo"
-                        className={cx("logo")}
-                      />
-                    </a>
-                    <h1 className={cx("heading")}>
-                      Đăng nhập vào Mỹ phẩm Mỹ Hạnh
-                    </h1>
-                    <p className={cx("description")}>
-                      Vui lòng đăng nhập tài khoản Thành Viên để xem ưu đãi và
-                      thanh toán dễ dàng hơn.
-                    </p>
-                  </header>
-
-                  <div className={cx("main")}>
-                    <Login />
-                  </div>
-                </div>
-              </div>
-            </div>
+            <iframe
+              src="https://accounts.regis.id.vn/login"
+              frameBorder="0"
+              className={cx("content__inner")}
+            ></iframe>
           </div>
         </div>
       )}
