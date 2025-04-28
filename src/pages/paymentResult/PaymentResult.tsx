ce811aee87e195992c11f17fdef1f515334b46ec
huusangcv <171899742+huusangcv@users.noreply.github.com>
2025-04-29 00:30:36 +0700

    fix: update result

--- src/pages/paymentResult/PaymentResult.tsx
+++ src/pages/paymentResult/PaymentResult.tsx
@@ -15,16 +15,18 @@ const PaymentResult = () => {
   const queryParams = useQueryParams();
   const queryString = queryParams.toString();
   const [paymentResult, setPaymentResult] = useState<string>('ĐẶT HÀNG THÀNH CÔNG');
-  const [isPaymentSuccess, setIsPaymentSuccess] = useState<boolean | null>(null); // Trạng thái giao dịch
-  const [isOnlinePayment, setIsOnlinePayment] = useState<boolean>(false); // Kiểm tra thanh toán online
+  const [isPaymentSuccess, setIsPaymentSuccess] = useState<boolean>(true); // Trạng thái giao dịch
 
   useEffect(() => {
     const fetchPaymentResult = async () => {
       try {
-        const res = await paymentMethods.getPaymentResult(queryString);
-        setPaymentResult(res.data.message);
-        setIsPaymentSuccess(res.data.success); // Trạng thái giao dịch
-        setIsOnlinePayment(res.data.isOnlinePayment); // Kiểm tra thanh toán online
+        const { data, status } = await paymentMethods.getPaymentResult(queryString);
+        if (status) {
+          setIsPaymentSuccess(true);
+        } else {
+          setIsPaymentSuccess(false);
+        }
+        setPaymentResult(data.message);
       } catch (error) {
         console.log(error);
         setPaymentResult('ĐÃ XẢY RA LỖI KHI XỬ LÝ GIAO DỊCH');
@@ -53,21 +55,14 @@ const PaymentResult = () => {
         )}
 
         <div className={cx('order-message')}>
-          {isOnlinePayment ? (
-            isPaymentSuccess ? (
-              <h3 className={cx('message-helper')}>
-                "Cảm ơn bạn đã thanh toán! Đơn hàng của bạn đã được xác nhận và chúng tôi sẽ xử lý ngay. Bạn sẽ nhận
-                được thông tin cập nhật qua email trong thời gian sớm nhất."
-              </h3>
-            ) : (
-              <h3 className={cx('message-helper')}>
-                "Thanh toán không thành công. Vui lòng thử lại hoặc liên hệ với chúng tôi để được hỗ trợ."
-              </h3>
-            )
+          {isPaymentSuccess ? (
+            <h3 className={cx('message-helper')}>
+              "Cảm ơn bạn đã thanh toán! Đơn hàng của bạn đã được xác nhận và chúng tôi sẽ xử lý ngay. Bạn sẽ nhận được
+              thông tin cập nhật qua email trong thời gian sớm nhất."
+            </h3>
           ) : (
             <h3 className={cx('message-helper')}>
-              "Cảm ơn bạn đã đặt hàng! Đơn hàng của bạn đã được xác nhận và chúng tôi sẽ xử lý ngay. Bạn sẽ nhận được
-              thông tin cập nhật qua email trong thời gian sớm nhất."
+              "Thanh toán không thành công. Vui lòng thử lại hoặc liên hệ với chúng tôi để được hỗ trợ."
             </h3>
           )}
         </div>
