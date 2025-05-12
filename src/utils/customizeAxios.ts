8bba99f3f7dd8be51be283117f2afeb58f096d40
huusangcv <171899742+huusangcv@users.noreply.github.com>
2025-05-12 21:42:42 +0700

    fix: change url domain

--- src/utils/customizeAxios.ts
+++ src/utils/customizeAxios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/v1/',
+  // baseURL: 'http://localhost:8080/v1/',
   // baseURL: 'https://backend.regis.id.vn/v1/',
-  // baseURL: 'https://backend.regis.id.vn/v1/',
+  baseURL: 'https://backend.regis.id.vn/v1/',
 });
 instance.defaults.timeout = 20000;
 // Add a request interceptor
